8d47cbc3c7c56eec329d74dc6440df13f40f5b89
Hai Ngo <ngodanghai9x@gmail.com>
2023-01-09 20:53:08 +0700

    refactor architecture micro service; omit, pick, partial func from swagger

--- src/micro-services/micro-services.module.ts
+++ src/micro-services/micro-services.module.ts
@@ -1,8 +1,50 @@
 import { Module } from '@nestjs/common';
-import { EmailSvModule } from './email-sv/email-sv.module';
+import { EmailSvService } from './email-sv/email-sv.service';
+import { EmailSvController } from './email-sv/email-sv.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientProxy, ClientsModule, ClientProxyFactory } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [EmailSvModule],
-  exports: [EmailSvModule],
+  imports: [
+    ConfigModule,
+    ClientsModule.registerAsync([
+      {
+        name: 'EMAIL_MICRO_SERVICE',
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => {
+          const host = configService.get('EMAIL_TCP_SV_HOST');
+          const port = configService.get('EMAIL_TCP_SV_PORT');
+          return {
+            transport: Transport.TCP,
+            options: {
+              host,
+              port,
+            },
+          };
+        },
+      },
+    ]),
+  ],
+  controllers: [EmailSvController],
+  providers: [
+    EmailSvService,
+    // {
+    //   provide: 'EMAIL_MICRO_SERVICE',
+    //   inject: [ConfigService],
+    //   useFactory: (configService: ConfigService) => {
+    //     const host = configService.get('EMAIL_TCP_SV_HOST');
+    //     const port = configService.get('EMAIL_TCP_SV_PORT');
+    //     return ClientProxyFactory.create({
+    //       transport: Transport.TCP,
+    //       options: {
+    //         host,
+    //         port,
+    //       },
+    //     });
+    //   },
+    // },
+  ],
+  exports: [EmailSvService],
 })
 export class MicroServicesModule {}
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,6 +1,8 @@
 // import { PartialType } from '@nestjs/mapped-types';
 // must nest g resource plural nouns
-import { PartialType } from '@nestjs/swagger';
+import { OmitType, PartialType, PickType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+// export class UpdateUserDto extends PartialType(CreateUserDto) {}
+// export class UpdateUserDto extends PickType(CreateUserDto, ['fullName']) {}
+export class UpdateUserDto extends PartialType(OmitType(CreateUserDto, ['password'])) {}
